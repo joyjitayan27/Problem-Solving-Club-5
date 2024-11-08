2a1ef10068a3a9b00a7d4ee318ac961625fbbc65
Joyjit Paul Ayan <145591350+joyjitayan27@users.noreply.github.com>
2024-11-08 20:48:49 +0600

    Add files via upload

---
+++ Week 2/Day 6/D_Epic_Transformation.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> cnt;
+        for (int i = 1; i <= n; i++)
+        {
+            int x;
+            cin >> x;
+            cnt[x]++;
+        }
+
+        priority_queue<int> pq;
+        for (auto val : cnt)
+        {
+            pq.push(val.second);
+        }
+
+        while (!pq.empty())
+        {
+            if (pq.size() < 2)
+            {
+                break;
+            }
+            int x, y;
+            x = pq.top();
+            pq.pop();
+            y = pq.top();
+            pq.pop();
+            x--, y--;
+            if (x >= 1)
+            {
+                pq.push(x);
+            }
+            if (y >= 1)
+            {
+                pq.push(y);
+            }
+        }
+
+        // int ans = 0;
+        // while (!pq.empty())
+        // {
+        //     ans += pq.top();
+        //     pq.pop();
+        // }
+        if (pq.empty())
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        cout << pq.top() << endl;
+
+        // cout << ans << '\n';
+    }
+
+    return 0;
+}
